9eb60d207e5eb85071d9049687bf0d16566a4609
bboInCoding17 <qhdla1004t@gmail.com>
2023-05-10 15:21:34 +0900

    올리기!

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -8,6 +8,12 @@ public class TestController {
 
 	@GetMapping("/")
 	public String index() {
+		System.out.println("회사에서 작업했3");
 		return "index";
 	}
+	@GetMapping("/test")
+	public String test() {
+		return "test";
+	}
+	
 }
